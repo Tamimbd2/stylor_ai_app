e62fdff58b5f9c8f91a6cadedb5c6825df91c225
Tamimbd2 <itstamim.ban@gmail.com>
2025-12-07 22:40:42 +0600

    add

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,16 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static final AUTH_LOGIN = _Paths.AUTH + _Paths.LOGIN;
+  static const SPLASH = _Paths.SPLASH;
+  static const ONBOARDING = _Paths.ONBOARDING;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const AUTH_LOGIN = '/login';
+  static const SPLASH = '/splash';
+  static const ONBOARDING = '/onboarding';
+}
