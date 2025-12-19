b01faf3f9470a4cd936beec4275c3013f440cc77
MAHABUB TAMIM <221002257@student.green.edu.bd>
2025-12-19 16:42:52 +0600

    update

--- lib/app/modules/auth/signup/views/signup_view.dart
+++ lib/app/modules/auth/signup/views/signup_view.dart
@@ -111,7 +111,7 @@ class _SignupViewState extends State<SignupView> {
                     focusNode: _nameFocus,
                     nextFocus: _emailFocus,
                     prefixIcon: Icons.person_outline,
-                    hintText: 'Mohammad Ali',
+                    hintText: 'Write Your Name',
                     keyboardType: TextInputType.name,
                     validator: (value) {
                       if (value == null || value.isEmpty) {
